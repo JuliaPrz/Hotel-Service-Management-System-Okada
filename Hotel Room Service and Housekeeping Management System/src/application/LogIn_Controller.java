a70edce749ea2cc4954e3f3d9b6e6b5ebde2692f
Julia Perez <julya.perez00@gmail.com>
2024-04-09 22:36:40 +0800

    Created the UI for the Log In Page

---
+++ Hotel Room Service and Housekeeping Management System/src/application/LogIn_Controller.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+public class LogIn_Controller implements Initializable {
+
+	@FXML
+	private ComboBox<String> logIn_cbb;
+	private String[] user = {"Guest", "Employee", "Admin"};
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		logIn_cbb.getItems().addAll(user);
+	}
+	
+	
+
+	
+}
