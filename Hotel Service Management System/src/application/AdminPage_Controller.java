e6d946a4e91a3e6d5194619440f5512034684cf2
Julia Perez <julya.perez00@gmail.com>
2024-04-17 11:32:52 +0800

    Add room pop up scene in Admin Page

--- Hotel Service Management System/src/application/AdminPage_Controller.java
+++ Hotel Service Management System/src/application/AdminPage_Controller.java
@@ -3,19 +3,30 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+
+
 
 public class AdminPage_Controller {
 	
+	// text area in Add Room Type
+	@FXML
+	private TextArea rType_description;
+	
+	@FXML
+	private TextArea rType_amenities;
+
+
 	@FXML
 	private AnchorPane dashboard_page, room_page, food_page, hk_page, acc_page, transact_page, profile_page;
 	
 	@FXML
 	private Button dashboard_btn, room_btn, food_btn, hk_btn, acc_btn, transact_btn, profile_btn;
 	
+	// Switches tab
 	@FXML
 	private void handleButtonAction(ActionEvent event) {
-		
 		// Get the ID of the source button
 	    String buttonId = ((Button) event.getSource()).getId();
 
@@ -50,10 +61,21 @@ public class AdminPage_Controller {
 	}
 	
 	
-	
-	
     public void initialize() {
 
+    	rType_description.setWrapText(true);
+    	rType_amenities.setWrapText(true);
+    	
+    
+
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
     	
     }
     
